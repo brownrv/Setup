41dae69bdf8e113983292d487527e77c47ade7f7
brownrv <brownrv@gmail.com>
2016-09-15 23:07:11 -0400

    Add link #5

--- Setup.R
+++ Setup.R
@@ -2,3 +2,4 @@
 # * http://r-pkgs.had.co.nz/git.html
 #   https://git-scm.com/book/en/v1/Git-Basics
 # * https://help.github.com/desktop/
+#   http://jeromyanglim.tumblr.com/post/63159223332/how-to-push-existing-git-rstudio-repository-to
